185900fb6f0d58967d0fbf103f3ff6f608aa7551
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-10-10 23:50:59 +0100

    conf

--- apps/ui-library/metro.config.js
+++ apps/ui-library/metro.config.js
@@ -3,6 +3,8 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+defaultConfig.watchFolders.push(path.resolve(__dirname, "../../packages/ui"));
+
 const withStorybook = require("@storybook/react-native/metro/withStorybook");
 
 module.exports = withStorybook(defaultConfig);
