470b4ebf39d1a3fd37a1a83d2dde1c4ae8e06f66
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-10-10 23:35:13 +0100

    expo router

--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,5 +1,22 @@
-import { Text } from "react-native";
+import { HomeIcon, HomeScreen } from "@acme/feature-home";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default function Page() {
-  return <Text>Top-level page</Text>;
+  return (
+    <View style={styles.container}>
+      <HomeIcon style={{ fontSize: 64 }} />
+      <HomeScreen />
+      <StatusBar style="auto" />
+    </View>
+  );
 }
